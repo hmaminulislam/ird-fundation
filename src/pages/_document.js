ba6dac1aa8ec995081b086a53ec1b2fb09c18284
hmaminulislam <mdaminulislamsahin14@gmail.com>
2023-11-07 01:28:25 +0600

    basic setup done

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Ird Fundation</title>
+        </Head>
       <body>
         <Main />
         <NextScript />
